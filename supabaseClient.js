49ad9f157040c88e1848fb423a8efc3cd393e7fc
poony01 <ponnyycarvalho8486@gmail.com>
2025-08-01 03:13:36 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js'
-
-const SUPABASE_URL = 'SUA_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'SUA_SUPABASE_ANON_KEY';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
